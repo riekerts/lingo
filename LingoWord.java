fabd901442a8cae7216284a166b73bce11d3ba20
nielsriekert <niels.riekert@gmail.com>
2016-11-29 21:11:12 +0100

    begin gemaakt door 2 classes aan te maken met enke methodes en instance variabelen

---
+++ LingoWord.java
@@ -0,0 +1,12 @@
+public class LingoWord {
+	private String word;
+	private String[] words = {"plaat", "talen", "vloer", "kopje"};
+
+	public void createNewWord() {
+
+	}
+
+	public String[] checkWord(String word) {
+		
+	}
+}
