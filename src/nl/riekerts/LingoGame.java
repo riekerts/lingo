f85dab89b4962cb9945308d7ec7b6485fe30e7f3
Arne Riekert <arneparne@gmail.com>
2016-12-01 16:57:09 +0100

    Komma toegevoegd hoera

--- src/nl/riekerts/LingoGame.java
+++ src/nl/riekerts/LingoGame.java
@@ -8,6 +8,6 @@ public class LingoGame {
 
 		String[] result = lingoWord.checkWord("vloer");
 
-		System.out.println(result[0] + ", " + result[1] + " " + result[2] + ", " + result[3] + ", " + result[4]);
+		System.out.println(result[0] + ", " + result[1] + ", " + result[2] + ", " + result[3] + ", " + result[4]);
 	}
 }
